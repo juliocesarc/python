dd0964717270c69d37f51f14c3e56872b3e0904c
juliocesarc <juliocostaesilva4@gmail.com>
2022-07-03 15:39:04 -0300

    aula22

---
+++ aula22/aula22.py
@@ -0,0 +1,8 @@
+saudacao = 'Opa'#input('Digíte aqui uma saudacao:')
+nome = 'Julio'#input('Digíte aqui seu nome')
+
+def funcao(saud, nom):
+    return f'{saud} {nom}'
+
+var = funcao(saudacao, nome)
+print(var)
