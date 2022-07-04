42cf47f5c424406f8d1e8725b688c833c5c86ea7
juliocesarc <juliocostaesilva4@gmail.com>
2022-07-03 21:22:17 -0300

    aula23

--- aula22/aula22.py
+++ aula22/aula22.py
@@ -1,8 +1,45 @@
-saudacao = 'Opa'#input('Digíte aqui uma saudacao:')
-nome = 'Julio'#input('Digíte aqui seu nome')
+# ex 01
 
-def funcao(saud, nom):
-    return f'{saud} {nom}'
+"""
+def saudacao(saudacao, nome):
+    print(f'{saudacao} {nome}')
 
-var = funcao(saudacao, nome)
-print(var)
+saudacao('Olá', 'João')
+"""
+
+# ex 02
+
+"""
+def soma(one, two, tree):
+    print(one + two + tree)
+
+soma(5, 3, 2)
+"""
+
+# ex 03
+
+"""
+def aumento_percentual(valor, percentual):
+    print(valor + (valor * percentual / 100))
+
+aumento_percentual(10, 10)
+aumento_percentual(100, 10)
+aumento_percentual(600, 10)
+"""
+
+# ex 04
+
+def fb(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return f'Fizz Buzz - (3 e 5) {n}'
+    elif n % 3 == 0:
+        return f'Fizz - (3) {n}'
+    elif n % 5 == 0:
+        return f'Buzz - (5) {n}'
+    return n 
+
+from random import randint
+
+for i in range(100):
+    aleatorio = randint(0, 100)
+    print(fb(aleatorio))
---
+++ aula23/aula23.py
@@ -0,0 +1,7 @@
+from pickle import NONE
+
+
+def func(a1, a2, a3, a4, a5, nome = None, a6)
+    print(a1, a2, a3, a4, a5, nome, a6)
+
+func(5, 6, 7, 8, 9, 'Julio', 1)
